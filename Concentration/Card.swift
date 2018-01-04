b982247603fbdd95eecff1e519dae1a510713d00
kc <kevinsketches@gmail.com>
2018-01-04 10:53:41 -0800

    Concentration
    updated

---
+++ Concentration/Card.swift
@@ -0,0 +1,27 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Kevin Chang on 1/3/18.
+//  Copyright © 2018 Kevin Chang. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return Card.identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
